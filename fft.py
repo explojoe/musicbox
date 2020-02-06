4949561842655673c142321edc06542069c2f82d
Joshua Wentzel <explojoe@yahoo.com>
2020-02-05 16:37:51 -0800

    clarified license

--- fft.py
+++ fft.py
@@ -1,4 +1,22 @@
-import scipy.ndimage.filters as fil
+#   Copyright (C) 2020, Joshua Wentzel
+#
+#   This program was written to converts audio files to a piano tone 
+#   locked version. This was part of a Oregon State University
+#   Junior Design Final Project for 2019-2020.
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <https://www.gnu.org/licenses/>.mport scipy.ndimage.filters as fil
+
 import scipy.signal as sp
 import matplotlib.pyplot as plt
 import numpy as np
